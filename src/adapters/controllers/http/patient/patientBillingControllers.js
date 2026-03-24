961cfff8a0d89220afedd1fd3d7118e420286c7c
hayamij <phuongtuan.dev@gmail.com>
2026-03-25 01:15:34 +0700

    update patientBillingControllers logic

--- src/adapters/controllers/http/patient/patientBillingControllers.js
+++ src/adapters/controllers/http/patient/patientBillingControllers.js
@@ -1,12 +1,17 @@
 import { createHandler } from '../createHandler.js';
-import { ViewBillingAndPaymentsViewModel } from '../../../viewmodels/patientViewModels.js';
+import { ViewBillingAndPaymentsViewModel, DownloadInvoiceViewModel } from '../../../viewmodels/patientViewModels.js';
 
-export function buildPatientBillingControllers({ viewBillingAndPaymentsUseCase }) {
+export function buildPatientBillingControllers({ viewBillingAndPaymentsUseCase, downloadInvoiceUseCase }) {
   return {
     viewBillingAndPayments: createHandler({
       useCase: viewBillingAndPaymentsUseCase,
       mapInput: (req) => ({ patientId: req.user?.id ?? req.query?.patientId, status: req.query?.status, page: req.query?.page, pageSize: req.query?.pageSize }),
       mapOutput: (result) => new ViewBillingAndPaymentsViewModel(result),
+    }),
+    downloadInvoice: createHandler({
+      useCase: downloadInvoiceUseCase,
+      mapInput: (req) => ({ invoiceId: req.params?.id, patientId: req.user?.id ?? req.query?.patientId }),
+      mapOutput: (result) => new DownloadInvoiceViewModel(result),
     }),
   };
 }
