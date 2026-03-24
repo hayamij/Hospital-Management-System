c2629817c5b95bbbc3790f3db25cf910b13ae4c7
hayamij <phuongtuan.dev@gmail.com>
2026-03-25 01:15:34 +0700

    update patientResponses logic

--- src/adapters/dto/response/patientResponses.js
+++ src/adapters/dto/response/patientResponses.js
@@ -8,6 +8,7 @@ export class RegisterPatientAccountResponse { constructor(vm) { Object.assign(th
 export class UpdatePatientProfileResponse { constructor(vm) { Object.assign(this, vm); } }
 export class ViewAppointmentsResponse { constructor(vm) { Object.assign(this, vm); } }
 export class ViewBillingAndPaymentsResponse { constructor(vm) { Object.assign(this, vm); } }
+export class DownloadInvoiceResponse { constructor(vm) { Object.assign(this, vm); } }
 export class ViewMedicalRecordsResponse { constructor(vm) { Object.assign(this, vm); } }
 export class DownloadPrescriptionResponse { constructor(vm) { Object.assign(this, vm); } }
 export class SendPatientMessageResponse { constructor(vm) { Object.assign(this, vm); } }
